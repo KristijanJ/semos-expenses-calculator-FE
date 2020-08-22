b32904bd79a1f06adf125a568eb8f774d92ed082
Kristijan Jovanovski <kristijan.j.mail@gmail.com>
2020-08-22 08:54:22 +0200

    convert login and register form to components

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import FormFieldInput from './FormFieldInput'
 
-export default function LoginForm() {
-  return (
-    <div className="auth-container">
-      <div className="form">
-        <FormFieldInput id="email" title="Email" type="email" />
-        <FormFieldInput id="password" title="Password" type="password" />
-        <FormFieldInput type="button" value="Sign In" />
+export default class LoginForm extends Component {
+  render() {
+    return (
+      <div className="auth-container">
+        <div className="form">
+          <FormFieldInput id="email" title="Email" type="email" />
+          <FormFieldInput id="password" title="Password" type="password" />
+          <FormFieldInput type="button" value="Sign In" />
+        </div>
+        <div className="form-field">
+        <p>Or if you don't have an account, <Link to="/register">Register</Link></p>
+        </div>
       </div>
-      <div className="form-field">
-      <p>Or if you don't have an account, <Link to="/register">Register</Link></p>
-      </div>
-    </div>
-  )
+    )
+  }
 }
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import FormFieldInput from './FormFieldInput'
 import FormFieldSelect from './FormFieldSelect'
 
-export default function RegisterForm() {
-  return (
-    <div className="auth-container">
-      <div className="form">
-       <FormFieldInput id="first-name" title="First Name" type="text" />
-       <FormFieldInput id="last-name" title="Last Name" type="text" />
-       <FormFieldInput id="email" title="Email" type="email" />
-       <FormFieldInput id="date-of-birth" title="Date of Birth" type="date" />
-       <FormFieldInput id="telephone" title="Telephone" type="text" />
-       <FormFieldSelect id="country" title="Country" type="text" />
-       <FormFieldInput id="password" title="Password" type="text" />
+export default class RegisterForm extends Component {
+  render() {
+    return (
+      <div className="auth-container">
+        <div className="form">
+        <FormFieldInput id="first-name" title="First Name" type="text" />
+        <FormFieldInput id="last-name" title="Last Name" type="text" />
+        <FormFieldInput id="email" title="Email" type="email" />
+        <FormFieldInput id="date-of-birth" title="Date of Birth" type="date" />
+        <FormFieldInput id="telephone" title="Telephone" type="text" />
+        <FormFieldSelect id="country" title="Country" type="text" />
+        <FormFieldInput id="password" title="Password" type="text" />
 
-       <FormFieldInput value="Register" type="button" />
+        <FormFieldInput value="Register" type="button" />
+        </div>
+        <div className="form-field">
+          <p>Or if you already have an account, <Link to="/login">Sign In</Link></p>
+        </div>
       </div>
-      <div className="form-field">
-        <p>Or if you already have an account, <Link to="/login">Sign In</Link></p>
-      </div>
-    </div>
-  )
+    )
+  }
 }
